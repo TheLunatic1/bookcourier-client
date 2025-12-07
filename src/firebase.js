ddfa3e33e768662af19431aeae3aa5739c766f00
Lunatic1 <112961007+TheLunatic1@users.noreply.github.com>
2025-12-08 03:27:57 +0600

    Deploy bug fixing.

--- src/firebase.js
+++ src/firebase.js
@@ -12,5 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-import { getAdditionalUserInfo } from "firebase/auth";
 export const auth = getAuth(app);
+export default app;
